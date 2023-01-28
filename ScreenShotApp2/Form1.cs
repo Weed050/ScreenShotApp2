5690914b712a72d39a3f4ad6b94ecf11fe5729b4
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-01-28 21:06:04 +0100

    you can overwrite path to directory in json file

--- ScreenShotApp2/Form1.cs
+++ ScreenShotApp2/Form1.cs
@@ -1,13 +1,18 @@
 using System.IO;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Text.Json.Nodes;
 
 namespace ScreenShotApp2
 {
     public partial class Form1 : Form
     {
-        public string directory;
+        public string directory1;
         public Form1()
         {
             InitializeComponent();
+            
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -17,21 +22,50 @@ namespace ScreenShotApp2
 
         private void button2_Click(object sender, EventArgs e)
         {
+           string strResultJson = string.Empty;
+            Variables1 variables = new Variables1()
+            {
+                
+            };
 
+            strResultJson = JsonConvert.SerializeObject(variables);
+            strResultJson = File.ReadAllText(@"variables.json");
+            //Variables1 resultVariables = JsonConvert.DeserializeObject<Variables1>(strResultJson);
+            //Console.WriteLine(resultVariables.ToString());
+
+            //Variables1 variables1 = new Variables1()
+            //{
+            //    z = z,
+            //    directory = directory
+            //};
+
+            strResultJson = JsonConvert.SerializeObject(variables);
+            File.WriteAllText(@"variables.json", strResultJson);
+            Console.WriteLine("stored!");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // pobranie lokalizacji do zapisania screen shota
             FolderBrowserDialog diag = new FolderBrowserDialog();
             if (diag.ShowDialog() == DialogResult.OK)
             {
                 textBoxDisplay.Text = diag.SelectedPath;
-                directory = diag.SelectedPath;
+                directory1 = diag.SelectedPath;
             }
             else
             {
                 textBoxDisplay.Text = "You didn't choose a folder";
             }
+            // zapisanie lokalizacji do pliku variables.json w
+            // C:\Users\User\source\repos\ScreenShotApp2\ScreenShotApp2\bin\Debug\net7.0-windows\
+            Variables1 variables1 = new Variables1()
+            {
+                directory = directory1
+            };
+            string strResultJson = string.Empty;
+            strResultJson = JsonConvert.SerializeObject(variables1);
+            File.WriteAllText(@"variables.json", strResultJson);
         }
     
     }
---
+++ ScreenShotApp2/variables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+
+namespace ScreenShotApp2
+{
+    public class Variables1
+    {
+        public int z { get; set; }
+        public string directory { get; set; }
+    }
+
+}
